cb7a075d7e475a77939315f2b612768a94bd912f
Harjot Singh Salh <harjot0124@gmail.com>
2024-01-30 23:11:40 -0700

    completed week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+    return (
+      <>
+        <p>Harjot Singh Salh <br/>
+        https://github.com/HarjotSalh/cprg306-assignments</p>
+      </>
+    )
+  }
